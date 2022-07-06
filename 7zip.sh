6cab20189b801ce439cad83167f98b9996ce215d
0xb0ba <vladimir.kharlamov@rt.ru>
2022-07-06 14:05:18 +0500

    remove artefacts

--- 7zip.sh
+++ 7zip.sh
@@ -6,6 +6,3 @@ date=$(date +%Y.%m.%d)
 rm "${name}-nopasaran-${date}.zip"
 
 zip -j "${name}-nopasaran-${date}.zip" out/*
-
-#zip -j -0 "${name}-nopasaran-${date}-update.zip" out/MT6735_Android_scatter.txt out/system.img
-
